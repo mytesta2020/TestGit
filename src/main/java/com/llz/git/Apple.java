30007a3f88d9bae73745be7f044c19c2824f20a4
eclipse_user <eclipse_user@163.com>
2019-03-05 22:56:53 +0800

    My First Commit

---
+++ src/main/java/com/llz/git/Apple.java
@@ -0,0 +1,20 @@
+/**
+* <p>FileName: Apple.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2018-2020</p>
+* <p>Company: www.llz.com</p>
+* @author Administrator
+* @date 2019年3月5日
+* @version 1.0
+*/
+package com.llz.git;
+
+/**
+ * <p>ClassName: Apple</p>
+ * <p>Description: </p>
+ * @author Administrator
+ * @date 2019年3月5日
+ */
+public class Apple {
+
+}
---
+++ src/test/java/com/llz/test/GoodTest.java
@@ -0,0 +1,20 @@
+/**
+* <p>FileName: GoodTest.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2018-2020</p>
+* <p>Company: www.llz.com</p>
+* @author Administrator
+* @date 2019年3月5日
+* @version 1.0
+*/
+package com.llz.test;
+
+/**
+ * <p>ClassName: GoodTest</p>
+ * <p>Description: </p>
+ * @author Administrator
+ * @date 2019年3月5日
+ */
+public class GoodTest {
+
+}
