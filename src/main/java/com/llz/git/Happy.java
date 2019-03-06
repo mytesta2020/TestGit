d417b031d1d56e94d4500013fd120d48f45f508e
eclipse_user <eclipse_user@163.com>
2019-03-06 14:27:08 +0800

    create a new class Happy.java

---
+++ src/main/java/com/llz/git/Happy.java
@@ -0,0 +1,20 @@
+/**
+* <p>FileName: Happy.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2018-2020</p>
+* <p>Company: www.llz.com</p>
+* @author Administrator
+* @date 2019年3月6日
+* @version 1.0
+*/
+package com.llz.git;
+
+/**
+ * <p>ClassName: Happy</p>
+ * <p>Description: </p>
+ * @author Administrator
+ * @date 2019年3月6日
+ */
+public class Happy {
+
+}
